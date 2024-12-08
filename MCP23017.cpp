fa7d2169973f70aaa177f93c3d79a999c9855941
flouis01 <flouis663@gmail.com>
2024-12-08 01:07:14 +0100

    Finished Basic functionality

--- MCP23017.cpp
+++ MCP23017.cpp
@@ -1,43 +1,170 @@
 #include "MCP23017.h"
 #include <Arduino.h>
 #include <Wire.h>
- 
+#include <math.h>
 
-MCP23017::MCP23017(MCP23017_config config) {
+//=============================================
+//    public functions
+//=============================================
+
+void MCP23017::begin(MCP23017_config config) {
+  //Set I²C Adress
   ADRESS = config.ADRESS;
 
-  ArrayCpy(GPA_MODE, config.GPA_MODE);
-  ArrayCpy(GPB_MODE, config.GPB_MODE);
-  char GPA_MODE_Byte = 0;
-  for (int i = 0; i < 7; i++) {
-    
+  // Reset MCP23017 
+  reset();
+
+  //Copy Pin Data
+  for (int i = 0; i < 8; i++) {
+    GPA_MODE[i] = config.GPA_MODE[i];
+    GPB_MODE[i] = config.GPB_MODE[i];
   }
-  Wire.beginTransmission(ADRESS);
-  Wire.write(0x0);
-  Wire.write(0x0);
-  Wire.endTransmission();
-  ArrayCpy(GPA_VALUE, config.GPA_VALUE);
-  ArrayCpy(GPB_VALUE, config.GPB_VALUE);
+  updatePinMode();
 }
 
-void MCP23017::pinMode(unsigned int pin, unsigned int mode) {
+void MCP23017::pinMode(uint8_t pin, uint8_t mode) {
+  // Input validation 
+  if(pin > 15) return;
+  if(mode > 2) return;
+  
+  if (pin < 8) {
+    GPA_MODE[pin] = mode;
+  } else {
+    GPB_MODE[pin-8] = mode;
+  }
+
+  updatePinMode();
 }
 
-void MCP23017::digitalWrite(unsigned int pin, unsigned int val) {
+void MCP23017::digitalWrite(uint8_t pin, uint8_t val) {
+  // Input validation 
+  pin = (pin > 15) ? 15 : pin;
+  val = (val >= 1) ? HIGH : LOW;
+
+  if (pin < 8) {
+    GPA_VALUE[pin] = val;
+    writeBit(GPIOA, pin, val);
+  } else {
+    GPA_VALUE[pin-8] = val;
+    writeBit(GPIOB, pin-8, val);
+  }
 }
 
-int MCP23017::digitalRead(unsigned int pin) {
+int MCP23017::digitalRead(uint8_t pin) {
+  // Input validation 
+  pin = (pin > 15) ? 15 : pin;
+
+  if (pin < 8) {
+    return (readByte(GPIOA) & (1 << pin)) != 0;
+  } else {
+    return (readByte(GPIOB) & (1 << (pin - 8))) != 0;
+  }
 }
 
-char MCP23017::readByte(char ByteAdress) {
+void MCP23017::reset() {
+  for (int i = 0; i <= 0x15; i++) {
+    writeByte(i, (i == IODIRA || i == IODIRB)?255:0);
+  }
+}
+
+void MCP23017::readAllMem() {
+  Serial.println("Reg. Name\tAddress (HEX)\tbit7\tbit6\tbit5\tbit4\tbit3\tbit2\tbit1\tbit0\tSum");
+  for (int i = 0; i <= 0x15; i++) {
+    byte data = 0x00;
+    Wire.beginTransmission(0x20);
+    Wire.write(i);
+    Wire.endTransmission();
+    Wire.requestFrom(0x20, 1);  // request one byte of data from MCP20317
+    data = Wire.read();
+    Serial.print(RegNames[i]);
+    Serial.print((strlen(RegNames[i]) > 7) ? "\t0x" : "\t\t0x");
+    Serial.print(i, HEX);
+    Serial.print("\t\t");
+    for (int i = 0; i < 8; i++) {
+      Serial.print((data & (1 << i)) != 0);
+      Serial.print("\t");
+    }
+    Serial.print(data);
+    Serial.println();
+  }
+}
+
+//=============================================
+//    private functions
+//=============================================
+
+void MCP23017::updatePinMode() {
+  byte data = 0;
+  byte ByteAdress = 0;
+
+  //GPA IO direction
+  ByteAdress = IODIRA;
+  data = 0;
+  for (int i = 0; i < 8; i++) {
+    if (GPA_MODE[i] != OUTPUT) {
+      data += 1 << i;
+    }
+  }
+  writeByte(ByteAdress, data);
+
+  //GPA General-Purpose-Pull-Up
+  ByteAdress = GPPUA;
+  data = 0;
+  for (int i = 0; i < 8; i++) {
+    if (GPA_MODE[i] == INPUT_PULLUP) {
+      data += 1 << i;
+    }
+  }
+  writeByte(ByteAdress, data);
+
+  //GPB IO direction
+  ByteAdress = IODIRB;
+  data = 0;
+  for (int i = 0; i < 8; i++) {
+    if (GPB_MODE[i] != OUTPUT) {
+      data += 1 << i;
+    }
+  }
+  writeByte(ByteAdress, data);
+
+  //GPB General-Purpose-Pull-Up
+  ByteAdress = GPPUB;
+  data = 0;
+  for (int i = 0; i < 8; i++) {
+    if (GPB_MODE[i] == INPUT_PULLUP) {
+      data += 1 << i;
+    }
+  }
+  writeByte(ByteAdress, data);
+}
+
+void MCP23017::writeBit(byte ByteAdress, uint8_t bitNr, uint8_t val) {
+  // Input validation 
+  val = (val >= 1) ? HIGH : LOW;
+  bitNr = (bitNr > 7) ? 7 : bitNr;
+
+  byte reg = 0;
+  byte currVal = 0;
+  reg = readByte(ByteAdress);
+  currVal = (reg & (1 << bitNr)) != 0;
+  if (currVal != val) {
+    //chage value
+    reg += (currVal < val) ? (1 << bitNr) : -(1 << bitNr);
+    writeByte(ByteAdress, reg);
+  }
+}
+
+int MCP23017::readByte(byte ByteAdress) {
   Wire.beginTransmission(ADRESS);
   Wire.write(ByteAdress);
   Wire.endTransmission();
-  return Wire.requestFrom(ADRESS, 1);
+  Wire.requestFrom(ADRESS, 1);
+  return Wire.read();
 }
 
-void MCP23017::ArrayCpy(unsigned int *Target[8], const unsigned int *A[8]) {
-  for (int i = 0; i < 8; i++) {
-    target[i] = A[i];
-  }
+void MCP23017::writeByte(byte ByteAdress, byte data) {
+  Wire.beginTransmission(ADRESS);
+  Wire.write(ByteAdress);
+  Wire.write(data);
+  Wire.endTransmission();
 }
--- MCP23017.h
+++ MCP23017.h
@@ -1,44 +1,131 @@
 #ifndef MCP23017_H
 #define MCP23017_H
- 
+#include <Arduino.h>
+
+
+// MCP23017 Regestry List
+#define IODIRA 0x00  //I/O DIRECTION REGISTER: 0=OUT; 1=IN
+#define IODIRB 0x01
+#define IPOLA 0x02  //INPUT POLARITY PORT REGISTER: 0=Logical equal; 1=Logical opposite
+#define IPOLB 0x03
+#define GPINTENA 0x04  //INTERRUPT-ON-CHANGE PINS: 0=Disabled; 1=Enabled
+#define GPINTENB 0x05
+#define DEFVALA 0x06  //DEFAULT VALUE REGISTER
+#define DEFVALB 0x07
+#define INTCONA 0x08  //INTERRUPT-ON-CHANGE CONTROL REGISTER
+#define INTCONB 0x09
+#define IOCON 0x0A  //I/O EXPANDER CONFIGURATION REGISTER
+//efine IOCON 0x0B
+#define GPPUA 0x0C  // GPIO PULL-UP RESISTOR REGISTER: 0=no Pullup; 1=Pullup
+#define GPPUB 0x0D
+#define INTFA 0x0E  //INTERRUPT FLAG REGISTER
+#define INTFB 0x0F
+#define INTCAPA 0x10  //INTERRUPT CAPTURED VALUE FOR PORT REGISTER
+#define INTCAPB 0x11
+#define GPIOA 0x12  //GENERAL PURPOSE I/O PORT REGISTER: 0=LOW; 1=HIGH
+#define GPIOB 0x13
+#define OLATA 0x14  //OUTPUT LATCH REGISTER: 0=LOW; 1=HIGH
+#define OLATB 0x15
+
+// MCP23017 Pin List
+#define GPA0 0
+#define GPA1 1
+#define GPA2 2
+#define GPA3 3
+#define GPA4 4
+#define GPA5 5
+#define GPA6 6
+#define GPA7 7
+#define GPB0 8
+#define GPB1 9
+#define GPB2 10
+#define GPB3 11
+#define GPB4 12
+#define GPB5 13
+#define GPB6 14
+#define GPB7 15
+
+
 typedef struct MCP23017_config {
   //Configured by the 3 Adress pins A0, A1 & A2
   //A0=GND & A1=GND & A2=GND -> Adress=0x20
   char ADRESS;
   //Defines default pin mode
   //0=INPUT; 1=OUTPUT; 2=INPUT_PULLUP
-  unsigned int GPA_MODE[8];
-  unsigned int GPB_MODE[8];
-  //Defines default values for the output pins
-  //0=LOW; 1=HIGH
-  unsigned int GPA_VALUE[8];
-  unsigned int GPB_VALUE[8];
+  uint8_t GPA_MODE[8];
+  uint8_t GPB_MODE[8];
 } MCP23017_config;
-
-enum {GPA0, GPA1, GPA2, GPA3, GPA4, GPA5, GPA6, GPA7, GPB0, GPB1, GPB2, GPB3, GPB4, GPB5, GPB6, GPB7};
 
 class MCP23017 {
 private:
   //Configured by the 3 Adress pins A0, A1 & A2
   //A0=GND & A1=GND & A2=GND -> Adress=0x20
   char ADRESS;
+
   //Defines default pin mode
   //0=INPUT; 1=OUTPUT; 2=INPUT_PULLUP
-  unsigned int GPA_MODE[8];
-  unsigned int GPB_MODE[8];
+  uint8_t GPA_MODE[8];
+  uint8_t GPB_MODE[8];
+
   //Defines default values for the output pins
   //0=LOW; 1=HIGH
-  unsigned int GPA_VALUE[8];
-  unsigned int GPB_VALUE[8];
+  uint8_t GPA_VALUE[8];
+  uint8_t GPB_VALUE[8];
 
-  char readByte(char ByteAdress);
-  void ArrayCpy(unsigned int *Target[8], const unsigned int *A[8]);
+  void updatePinMode();
+
+  //names of all memory-registers (Used by the ReadAllMem function)
+  const char RegNames[22][10] = {
+    "IODIRA",
+    "IODIRB",
+    "IPOLA",
+    "IPOB",
+    "GPINTENA",
+    "GPINTENB",
+    "DEFVALA",
+    "DEFVALB",
+    "INTCONA",
+    "INTCONB",
+    "IOCON",
+    "IOCON",
+    "GPPUA",
+    "GPPUB",
+    "INTFA",
+    "INTFB",
+    "INTCAPA",
+    "INTCAPB",
+    "GPIOA",
+    "GPIOB",
+    "OLATA",
+    "OLATB"
+  };
+
+  void writeBit(byte ByteAdress, uint8_t bitNr, uint8_t val);
+
+  int readByte(byte ByteAdress);
+
+  void writeByte(byte ByteAdress, byte data);
+
 
 public:
-  MCP23017(MCP23017_config config);
-  void pinMode(unsigned int pin, unsigned int mode);
-  void digitalWrite(unsigned int pin, unsigned int val);
-  int digitalRead(unsigned int pin);
+
+  // Begins comunication with the MCP23017, resets the MCP23017 and make basic Settings (Pinmode)
+  void begin(MCP23017_config config);
+
+  // Changes the pinmode of a specified pin
+  void pinMode(uint8_t pin, uint8_t mode);
+
+  // Sets the digital output of a specified pin
+  void digitalWrite(uint8_t pin, uint8_t val);
+
+  // Returns the digital high low state of a specified pin
+  int digitalRead(uint8_t pin);
+
+  // Reads all memory-registers and prints them to the Serial Monitor in a table format
+  void readAllMem();
+
+  // Resets the Chip
+  void reset();
 };
 
 #endif
